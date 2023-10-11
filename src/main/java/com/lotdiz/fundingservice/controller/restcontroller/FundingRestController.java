70f139d0a981d422736470428b7cfad517b0f32f
binarywoo27 <blu_mountain@naver.com>
2023-10-11 10:00:48 +0900

    refactor: add /api to restcontroller

--- src/main/java/com/lotdiz/fundingservice/controller/restcontroller/FundingRestController.java
+++ src/main/java/com/lotdiz/fundingservice/controller/restcontroller/FundingRestController.java
@@ -10,29 +10,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class FundingRestController {
-  private final FundingService fundingService;
+private final FundingService fundingService;
 
-  @PostMapping("/projects/{projectId}/fundings")
-  public ResponseEntity<SuccessResponse> createFunding(
-      @RequestBody CreateFundingRequest createFundingRequest) {
-    // Funding, ProductFunding 저장.
-    Long fundingId = fundingService.createFunding(createFundingRequest);
+@PostMapping("/projects/{projectId}/fundings")
+public ResponseEntity<SuccessResponse<Map<String, GetFundingDetailResponse>>> createFunding(
+        @RequestBody CreateFundingRequest createFundingRequest) {
+  // Funding, ProductFunding 저장.
+  Long fundingId = fundingService.createFunding(createFundingRequest);
 
-    GetFundingDetailResponse getFundingDetailResponse =
-        fundingService.getFundingDetailResponse(fundingId);
+  GetFundingDetailResponse getFundingDetailResponse =
+          fundingService.getFundingDetailResponse(fundingId);
 
-    return ResponseEntity.ok()
-        .body(
-            SuccessResponse.builder()
-                .code(String.valueOf(HttpStatus.OK.value()))
-                .message(HttpStatus.OK.name())
-                .detail("펀딩 성공")
-                .data(Map.of("fundingDetail", getFundingDetailResponse))
-                .build());
-  }
+  return ResponseEntity.ok()
+          .body(
+                  SuccessResponse.<Map<String, GetFundingDetailResponse>>builder()
+                          .code(String.valueOf(HttpStatus.OK.value()))
+                          .message(HttpStatus.OK.name())
+                          .detail("펀딩 성공")
+                          .data(Map.of("fundingDetail", getFundingDetailResponse))
+                          .build());
 }
+}
+
+
