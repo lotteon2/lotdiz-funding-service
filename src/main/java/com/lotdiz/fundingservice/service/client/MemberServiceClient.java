9619962f133a55bc670c5493652c62874aceb758
binarywoo27 <blu_mountain@naver.com>
2023-10-17 15:15:55 +0900

    fix: change method to PutMapping

--- src/main/java/com/lotdiz/fundingservice/service/client/MemberServiceClient.java
+++ src/main/java/com/lotdiz/fundingservice/service/client/MemberServiceClient.java
@@ -3,12 +3,11 @@ package com.lotdiz.fundingservice.service.client;
 import com.lotdiz.fundingservice.dto.request.MemberPointUpdateRequestDto;
 import com.lotdiz.fundingservice.utils.SuccessResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="memberServiceClient", url="${endpoint.member-service}")
 public interface MemberServiceClient {
-  @PostMapping("/members/use-point")
+  @PutMapping("/members/use-point")
   SuccessResponse usePoint(@RequestBody MemberPointUpdateRequestDto memberPointUpdateRequestDto);
 }
